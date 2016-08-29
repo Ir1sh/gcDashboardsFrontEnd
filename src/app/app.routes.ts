713b3375097932c7e54ebde390180e6efc5a3133
Ir1sh <mmoore@iofficecorp.com>
2016-08-28 19:04:09 -0500

    Revert "Revert "merged upstream""
    This reverts commit 5927001b0b3d440b2569dc4272defe25f9a7f36a.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { WebpackAsyncRoute } from '@angularclass/webpack-toolkit';
-import { RouterConfig } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { Home } from './home';
 import { AlternativeCourses} from './alternative-courses'
 import {  AlternativeCoursesResults } from './alternative-courses/results'
@@ -18,7 +17,8 @@ import { NoContent } from './no-content';
 
 import { DataResolver } from './app.resolver';
 
-export const routes: RouterConfig = [
+
+export const ROUTES: Routes = [
   { path: '',      component: Home },
   { path: 'home',  component: Home },
     {
@@ -83,26 +83,3 @@ export const routes: RouterConfig = [
     ]},
   { path: '**',    component: NoContent },
 ];
-
-// Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-// asyncRoutes is needed for our @angularclass/webpack-toolkit that will allow us to resolve
-// the component correctly
-
-export const asyncRoutes: AsyncRoutes = {
-  // we have to use the alternative syntax for es6-promise-loader to grab the routes
-  'About': require('es6-promise-loader!./about'),
-  'Detail': require('es6-promise-loader!./+detail'),
-  'Index': require('es6-promise-loader!./+detail'), // must be exported with detail/index.ts
-};
-
-
-// Optimizations for initial loads
-// An array of callbacks to be invoked after bootstrap to prefetch async routes
-export const prefetchRouteCallbacks: Array<IdleCallbacks> = [
-  asyncRoutes['About'],
-  asyncRoutes['Detail'],
-   // es6-promise-loader returns a function
-];
-
-
-// Es6PromiseLoader and AsyncRoutes interfaces are defined in custom-typings
